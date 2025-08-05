2a78a56627a64f62d06d46690f95aaed2c08ec4b
omkarxo <92944253+Omixo@users.noreply.github.com>
2025-08-05 10:38:18 +0530

    Update Oops.java
    added withdrawl and deposit

--- Oops/Oops.java
+++ Oops/Oops.java
@@ -1,29 +1,41 @@
-class Bank
-  {
+class Bank {
+    int balance;
 
-int balance;
-public Bank(int b){
-    this.balance = b;
-}
-
-@Override
-    public String toString(){
-        return("i m from toString");
+    public Bank(int b) {
+        this.balance = b;
     }
 
+    public void deposit(int amount) {
+        balance = balance + amount;
+        System.out.println("Deposited: $" + amount);
+    }
 
-    public void showBalance (){
-    System.out.println("your Balance :100$k"+" "+  this.balance);
+    public void withdraw(int amount) {
+        balance = balance - amount;
+        System.out.println("Withdrawn: $" + amount);
+    }
+
+    public void showBalance() {
+        System.out.println("Your Balance: $" + balance);
+    }
+
+    @Override
+    public String toString() {
+        return "I'm from toString";
+    }
 }
-}
-    
-
-
 
 public class Oops {
-public static void main(String[] args) {
-    Bank b = new Bank(2000);
-    System.out.println(b);
-    b.showBalance();
-}    
+    public static void main(String[] args) {
+        Bank b = new Bank(2000);
+
+        System.out.println(b);       
+        b.showBalance();             
+
+        b.deposit(5000);              
+        b.showBalance();             
+
+        b.withdraw(3000);            
+        b.showBalance();            
+    }
 }
